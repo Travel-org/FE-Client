9ebd73467f20e18115a90668824dcdcd88f69be6
Kyungbin <pkb8839@naver.com>
2022-10-17 16:38:15 +0900

    [Fix]: change message

--- trave-org/src/services/schedule.ts
+++ trave-org/src/services/schedule.ts
@@ -1,28 +1,30 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: API_URL }),
+// export const api = createApi({
+//   baseQuery: fetchBaseQuery({ baseUrl: API_URL }),
 
-  tagTypes: ["schedule"],
+//   tagTypes: ["schedule"],
 
-  endpoints: (build) => ({
-    createSchedule: build.mutation({
-      query: (arg: number) => ({
-        url: `/v1/travels/${arg}/schedules`,
-        method: "POST",
-      }),
-      onQueryStarted: async (arg, { dispatch, queryFulfilled }) => {
-        const updateResponse = await queryFulfilled;
-        dispatch(
-          api.util.updateQueryData("getSchedule", arg, (draft) => {
-            draft.push(updateResponse);
-          })
-        );
-      },
-    }),
-    getSchedule: build.query<any[], number>({
-      query: (arg) => ({ url: `/v1/travels/${arg}/schedules`, method: "GET" }),
-      providesTags: ["schedule"],
-    }),
-  }),
-});
+//   endpoints: (build) => ({
+//     createSchedule: build.mutation({
+//       query: (arg: string) => ({
+//         url: `/v1/travels/${arg}/schedules`,
+//         method: "POST",
+//       }),
+
+//       onQueryStarted: async (arg, { dispatch, queryFulfilled }) => {
+//         const updateResponse = await queryFulfilled;
+
+//         dispatch(
+//           api.util.updateQueryData("getSchedule", arg, (draft) => {
+//             draft.push(updateResponse);
+//           })
+//         );
+//       },
+//     }),
+//     getSchedule: build.query<any[], string>({
+//       query: (arg) => ({ url: `/v1/travels/${arg}/schedules`, method: "GET" }),
+//       providesTags: ["schedule"],
+//     }),
+//   }),
+// });
