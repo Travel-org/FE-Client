01cbd572891bbec359f507aed005d9cf7410d4ac
Kyungbin <pkb8839@naver.com>
2022-10-26 22:32:47 +0900

    [Feat]: create post api

--- trave-org/src/app/api/postApi.ts
+++ trave-org/src/app/api/postApi.ts
@@ -1,23 +1,32 @@
 import baseApi, { IPaginationResponse } from "@src/app/api/baseApi";
 import { POST_BASE_URL } from "@utils/type";
-import { IPostResponse } from "@src/app/api/api";
-
+import { IPageRequest, IPostResponse } from "@src/app/api/api";
 const postApi = baseApi
   .enhanceEndpoints({
     addTagTypes: ["Post"],
   })
   .injectEndpoints({
     endpoints: (builder) => ({
-      getPostsOfFriends: builder.query<IPaginationResponse<IPostResponse>, void>({
-        query: () => ({
-          url: POST_BASE_URL,
+      getPostsOfFriends: builder.query<
+        IPaginationResponse<IPostResponse>,
+        IPageRequest
+      >({
+        query: (pageRequest) => ({
+          url:
+            POST_BASE_URL +
+            `?pageNumber=${pageRequest.pageNumber}&pageSize=${pageRequest.pageSize}`,
           method: "GET",
         }),
-        providesTags: (result, error, postId) => [
-          { type: "Post", id: "LIST" },
-        ]
-      })
-    })
+        providesTags: (result, error, postId) => [{ type: "Post", id: "LIST" }],
+      }),
+      createPost: builder.mutation<any, FormData>({
+        query: (formData) => ({
+          url: POST_BASE_URL,
+          method: "POST",
+          body: formData,
+        }),
+      }),
+    }),
   });
 
 export default postApi;
