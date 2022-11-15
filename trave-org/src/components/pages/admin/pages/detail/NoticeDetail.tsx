c451a22ebaf95dcf7147431cd7ef3f67175cbfc2
Kyungbin <pkb8839@naver.com>
2022-11-15 21:43:07 +0900

    [Fix]:fix notice image style

--- trave-org/src/components/pages/admin/pages/detail/NoticeDetail.tsx
+++ trave-org/src/components/pages/admin/pages/detail/NoticeDetail.tsx
@@ -3,39 +3,39 @@ import noticeApi from "@src/app/api/noticeApi";
 import { useParams } from "react-router-dom";
 
 const NoticeContainer = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    padding: 0px 10px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 0px 10px;
 `;
 const NoticeHeader = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: end;
-    border-bottom: 1px solid gray;
-    padding-bottom: 5px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+  border-bottom: 1px solid gray;
+  padding-bottom: 5px;
 `;
 
 const NoticeTitle = styled.p`
-    width: 80%;
-    font-size: 30px;
-    font-weight: 600;
+  width: 80%;
+  font-size: 30px;
+  font-weight: 600;
 `;
 
 const NoticeHeaderColumn = styled.div`
-    width: 20%;
-    display: flex;
-    flex-direction: column;
-    align-items:end;
-`
+  width: 20%;
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+`;
 
 const NoticeAuthor = styled.div`
-    color: black;
-`
+  color: black;
+`;
 
 const NoticeCreatedAt = styled.div`
-    color: gray;
+  color: gray;
 `;
 
 const NoticePhoto = styled.div<{ img: string }>`
@@ -46,42 +46,37 @@ const NoticePhoto = styled.div<{ img: string }>`
 `;
 
 const NoticeContent = styled.p`
-    width: 100%;
+  width: 100%;
 `;
 
 const NoticeDetail = () => {
+  const { noticeId } = useParams<"noticeId">();
 
-    const { noticeId } = useParams<"noticeId">();
+  const { data: noticeData } = noticeApi.useGetNoticeQuery(noticeId as string);
 
-    const {data: noticeData} = noticeApi.useGetNoticeQuery(noticeId as string);
-
-    console.log(noticeData);
-    return (
-        <NoticeContainer>
-            <NoticeHeader>
-                <NoticeTitle>
-                    {noticeData?.title}
-                </NoticeTitle>
-                <NoticeHeaderColumn>
-                    <NoticeAuthor>
-                        {noticeData?.authorInfo.userName}
-                    </NoticeAuthor>
-                    <NoticeCreatedAt>
-                        {noticeData?.createdAt.substring(0, 10)}
-                    </NoticeCreatedAt>
-                </NoticeHeaderColumn>
-            </NoticeHeader>
-            {noticeData !== undefined &&
-            noticeData.photoInfos.map(
-                (photo) => (
-                    <NoticePhoto img={photo.name}></NoticePhoto>
-                )
-            )}
-            <NoticeContent>
-                {noticeData?.content}
-            </NoticeContent>
-        </NoticeContainer>
-    )
-}
+  console.log(noticeData);
+  return (
+    <NoticeContainer>
+      <NoticeHeader>
+        <NoticeTitle>{noticeData?.title}</NoticeTitle>
+        <NoticeHeaderColumn>
+          <NoticeAuthor>{noticeData?.authorInfo.userName}</NoticeAuthor>
+          <NoticeCreatedAt>
+            {noticeData?.createdAt.substring(0, 10)}
+          </NoticeCreatedAt>
+        </NoticeHeaderColumn>
+      </NoticeHeader>
+      {noticeData !== undefined &&
+        noticeData.photoInfos.map((photo, idx) => (
+          <img
+            key={idx}
+            src={photo.name}
+            style={{ maxWidth: "800px", width: "100%" }}
+          ></img>
+        ))}
+      <NoticeContent>{noticeData?.content}</NoticeContent>
+    </NoticeContainer>
+  );
+};
 
 export default NoticeDetail;
